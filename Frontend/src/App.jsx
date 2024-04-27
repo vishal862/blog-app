9695adf85f86db1ae5a0230dec5759ef1046f68b
“Vishal <vishaldevkate1981@gmail.com>
2024-04-27 18:58:23 +0530

    created pages and routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import {BrowserRouter ,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
 
 export default function App() {
   return (
-    <div className='bg-red-900'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/sign-up' element={<Signup/>}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
