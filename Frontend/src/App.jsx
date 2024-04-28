76f7c2436d6fe72e8fb312204353acd8c0946a34
“Vishal <vishaldevkate1981@gmail.com>
2024-04-28 16:17:52 +0530

    created header section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,10 +6,12 @@ import About from './pages/About'
 import Projects from './pages/Projects'
 import Signup from './pages/Signup'
 import Signin from './pages/Signin'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
