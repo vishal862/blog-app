f683f3e3abb81c963c2141284259737b280a7283
“Vishal <vishaldevkate1981@gmail.com>
2024-04-28 17:24:02 +0530

    db connection changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config();
 
-app.get("/",(req,res)=>{
-    res.send("hello")
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('⚙️ mongodb connected');
+}).catch((error)=>{
+    console.log("connection error❌");
 })
 
 app.listen(3000,()=> {
