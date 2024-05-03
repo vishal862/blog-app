b02e0d07987661ba0709ba4172805b3588f717cc
“Vishal <vishaldevkate1981@gmail.com>
2024-05-03 19:31:52 +0530

    signin api route

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,31 +1,76 @@
 import { User } from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signUp =async(req,res,next)=>{
+
+export const signUp = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (
+      !username ||
+      !password ||
+      !email ||
+      username === "" ||
+      email === "" ||
+      password === ""
+    ) {
+      next(errorHandler(400, "All Fields are required!"));
+    }
+
+    const existedUser = await User.findOne({ username, email });
+
+    if (existedUser) {
+      next(errorHandler(400, "User Exists!"));
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(200).json({ message: "user created successfully", user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res,next) => {
+    const {email,password} = req.body;
+
+        if(!email || !password || email === "" || password === ""){
+             next(errorHandler(400,'email or password is missing'))
+        }
+
     try {
-        const {username,email,password} = req.body;
-    
-        if(!username || !password || !email || username === '' || email === '' || password === ''){
-            next(errorHandler(400,'All Fields are required!'))
-        }
-    
-        const existedUser = await User.findOne({username,email});
-    
-        if(existedUser){
-            next(errorHandler(400,'User Exists!'))
+        const validUser = await User.findOne({email})
+
+        if(!validUser){
+            return next(errorHandler(404,"user not found"))
         }
 
-        const hashedPassword = await bcryptjs.hash(password,10)
-    
-        const user = await User.create({
-            username,
-            email,
-            password : hashedPassword
-        })
-    
-        return res.status(200).json({message:'user created successfully',user})
+        const hashRemovedPassword = bcryptjs.compareSync(password,validUser.password)
+
+        if(!hashRemovedPassword){
+            return next(errorHandler(400,"Wrong Password!"))
+        }
+
+        const token = jwt.sign({id : validUser._id},process.env.JWT_SECRET)
+
+        const {password : pass , ...rest} = validUser._doc;
+
+        //This is using object destructuring and the rest/spread operator. It's extracting the password property from the validUser._doc object and assigning it to a variable named pass. The rest of the properties of validUser._doc (excluding password) are collected into a new object named rest.
+
+        res.status(200).cookie("access_token",token,{
+            httpOnly : true,
+            secure : true
+        }).json(rest)
+
     } catch (error) {
         next(error);
     }
-}
+};
